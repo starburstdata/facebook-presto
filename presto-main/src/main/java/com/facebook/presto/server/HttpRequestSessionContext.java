848f0ddb03049107fb7696b0ec834c5d3f158d4b
Christina Wallin <christina.wallin@teradata.com>
2016-08-05 16:43:18 -0400

    Pass prepared statements in HTTP request body
    HTTP request headers have a 64k limit, by default 4k in Jetty.
    Prepared statements can be much longer than that, so if the header
    "X-Presto-Prepared-Statement-In-Body" is specified, the server
    assumes that the query has been sent as a Jackson-serialized QuerySubmission
    and parses the request appropriately.
    
    In order to preserve backwards compatibility for clients, it is still
    possible to send a raw query string if the above header is not sent.
    
    Only /v1/statement supports sending the query via a QuerySubmission object,
    since /v1/query and /v1/execute don't support prepared statements.

--- presto-main/src/main/java/com/facebook/presto/server/HttpRequestSessionContext.java
+++ presto-main/src/main/java/com/facebook/presto/server/HttpRequestSessionContext.java
@@ -93,7 +93,7 @@ public final class HttpRequestSessionContext
     private final boolean clientTransactionSupport;
     private final String clientInfo;
 
-    public HttpRequestSessionContext(HttpServletRequest servletRequest)
+    public HttpRequestSessionContext(HttpServletRequest servletRequest, Map<String, String> additionalPreparedStatements)
             throws WebApplicationException
     {
         catalog = trimEmptyToNull(servletRequest.getHeader(PRESTO_CATALOG));
@@ -147,7 +147,7 @@ public final class HttpRequestSessionContext
         this.catalogSessionProperties = catalogSessionProperties.entrySet().stream()
                 .collect(toImmutableMap(Entry::getKey, entry -> ImmutableMap.copyOf(entry.getValue())));
 
-        preparedStatements = parsePreparedStatementsHeaders(servletRequest);
+        preparedStatements = parsePreparedStatementsHeaders(servletRequest, additionalPreparedStatements);
 
         String transactionIdHeader = servletRequest.getHeader(PRESTO_TRANSACTION_ID);
         clientTransactionSupport = transactionIdHeader != null;
@@ -321,7 +321,7 @@ public final class HttpRequestSessionContext
         }
     }
 
-    private static Map<String, String> parsePreparedStatementsHeaders(HttpServletRequest servletRequest)
+    private static Map<String, String> parsePreparedStatementsHeaders(HttpServletRequest servletRequest, Map<String, String> additionalPreparedStatements)
     {
         ImmutableMap.Builder<String, String> preparedStatements = ImmutableMap.builder();
         for (String header : splitSessionHeader(servletRequest.getHeaders(PRESTO_PREPARED_STATEMENT))) {
@@ -349,6 +349,7 @@ public final class HttpRequestSessionContext
 
             preparedStatements.put(statementName, sqlString);
         }
+        preparedStatements.putAll(additionalPreparedStatements);
         return preparedStatements.build();
     }
 
