f35db281ffe503c2abf12ff79ff3c88e335ce0af
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-10-07 15:03:39 +0200

    Allow empty partitions for clustered hive tables

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveClientConfig.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveClientConfig.java
@@ -122,6 +122,7 @@ public class HiveClientConfig
     private HdfsAuthenticationType hdfsAuthenticationType = HdfsAuthenticationType.NONE;
     private boolean hdfsImpersonationEnabled;
     private boolean multiFileBucketingEnabled;
+    private boolean emptyBucketedPartitionsEnabled;
 
     private boolean skipDeletionForAlter;
 
@@ -1056,6 +1057,19 @@ public class HiveClientConfig
     public HiveClientConfig setMultiFileBucketingEnabled(boolean multiFileBucketingEnabled)
     {
         this.multiFileBucketingEnabled = multiFileBucketingEnabled;
+        return this;
+    }
+
+    public boolean isEmptyBucketedPartitionsEnabled()
+    {
+        return emptyBucketedPartitionsEnabled;
+    }
+
+    @Config("hive.empty-bucketed-partitions.enabled")
+    @ConfigDescription("Allow partitions without files for clustered table")
+    public HiveClientConfig setEmptyBucketedPartitionsEnabled(boolean emptyBucketedPartitionsEnabled)
+    {
+        this.emptyBucketedPartitionsEnabled = emptyBucketedPartitionsEnabled;
         return this;
     }
 }
