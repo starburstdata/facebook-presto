fab8e2b3b42988727af1c19692ee6e47262d40c5
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-10-18 11:03:21 -0400

    Secured internal communication with LDAP product tests

--- presto-product-tests/bin/run_on_docker.sh
+++ presto-product-tests/bin/run_on_docker.sh
@@ -123,6 +123,8 @@ if [[ "$ENVIRONMENT" == "multinode" ]]; then
    PRESTO_SERVICES="${PRESTO_SERVICES} presto-worker"
 elif [[ "$ENVIRONMENT" == "multinode-tls" ]]; then
    PRESTO_SERVICES="${PRESTO_SERVICES} presto-worker-1 presto-worker-2"
+elif [[ "$ENVIRONMENT" == "multinode-tls-ldap" ]]; then
+   PRESTO_SERVICES="${PRESTO_SERVICES} presto-worker-1 presto-worker-2 ldapserver"
 fi
 
 # check docker and docker compose installation
