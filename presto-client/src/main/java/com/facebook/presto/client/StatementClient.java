zz22394/presto

Support long prepared statements in QueryResults

presto-client/src/main/java/com/facebook/presto/client/StatementClient.java
@@ -47,10 +47,9 @@ import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
-import static com.facebook.presto.client.PrestoHeaders.PRESTO_ADDED_PREPARE;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_CLEAR_SESSION;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_CLEAR_TRANSACTION_ID;
-import static com.facebook.presto.client.PrestoHeaders.PRESTO_DEALLOCATED_PREPARE;
+import static com.facebook.presto.client.PrestoHeaders.PRESTO_PREPARED_STATEMENT_IN_BODY;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_SET_SESSION;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_STARTED_TRANSACTION_ID;
 import static com.google.common.base.MoreObjects.firstNonNull;
@@ -246,6 +245,7 @@ public class StatementClient
     {
         builder.setHeader(PrestoHeaders.PRESTO_USER, user);
         builder.setHeader(USER_AGENT, USER_AGENT_VALUE)
+                .setHeader(PRESTO_PREPARED_STATEMENT_IN_BODY, "true")
                 .setUri(nextUri);
 
         return builder;
@@ -320,17 +320,6 @@ public class StatementClient
             resetSessionProperties.add(clearSession);
         }
 
-        for (String entry : response.getHeaders(PRESTO_ADDED_PREPARE)) {
-            List<String> keyValue = SESSION_HEADER_SPLITTER.splitToList(entry);
-            if (keyValue.size() != 2) {
-                continue;
-            }
-            addedPreparedStatements.put(urlDecode(keyValue.get(0)), urlDecode(keyValue.get(1)));
-        }
-        for (String entry : response.getHeaders(PRESTO_DEALLOCATED_PREPARE)) {
-            deallocatedPreparedStatements.add(urlDecode(entry));
-        }
-
         String startedTransactionId = response.getHeader(PRESTO_STARTED_TRANSACTION_ID);
         if (startedTransactionId != null) {
             this.startedtransactionId.set(startedTransactionId);
@@ -339,7 +328,12 @@ public class StatementClient
             clearTransactionId.set(true);
         }
 
-        currentResults.set(response.getValue());
+        QueryResults queryResults = response.getValue();
+
+        this.addedPreparedStatements.putAll(queryResults.getAddedPreparedStatements());
+        this.deallocatedPreparedStatements.addAll(queryResults.getDeallocatedPreparedStatements());
+
+        currentResults.set(queryResults);
     }
 
     private RuntimeException requestFailedException(String task, Request request, JsonResponse<QueryResults> response)

starburstdata/facebook-presto

presto-client/src/main/java/com/facebook/presto/client/StatementClient.java
@@ -51,7 +51,7 @@ import static com.facebook.presto.client.PrestoHeaders.PRESTO_CLEAR_TRANSACTION_
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_CLIENT_INFO;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_DEALLOCATED_PREPARE;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_LANGUAGE;
-import static com.facebook.presto.client.PrestoHeaders.PRESTO_PREPARED_STATEMENT;
+import static com.facebook.presto.client.PrestoHeaders.PRESTO_PREPARED_STATEMENT_IN_BODY;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_SCHEMA;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_SESSION;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_SET_CATALOG;
@@ -280,6 +280,7 @@ public class StatementClient
         return new Request.Builder()
                 .addHeader(PRESTO_USER, user)
                 .addHeader(USER_AGENT, USER_AGENT_VALUE)
+                .addHeader(PRESTO_PREPARED_STATEMENT_IN_BODY, "true")
                 .url(url);
     }
 
@@ -353,17 +354,6 @@ public class StatementClient
         }
         resetSessionProperties.addAll(headers.values(PRESTO_CLEAR_SESSION));
 
-        for (String entry : headers.values(PRESTO_ADDED_PREPARE)) {
-            List<String> keyValue = SESSION_HEADER_SPLITTER.splitToList(entry);
-            if (keyValue.size() != 2) {
-                continue;
-            }
-            addedPreparedStatements.put(urlDecode(keyValue.get(0)), urlDecode(keyValue.get(1)));
-        }
-        for (String entry : headers.values(PRESTO_DEALLOCATED_PREPARE)) {
-            deallocatedPreparedStatements.add(urlDecode(entry));
-        }
-
         String startedTransactionId = headers.get(PRESTO_STARTED_TRANSACTION_ID);
         if (startedTransactionId != null) {
             this.startedTransactionId.set(startedTransactionId);
@@ -371,6 +361,9 @@ public class StatementClient
         if (headers.get(PRESTO_CLEAR_TRANSACTION_ID) != null) {
             clearTransactionId.set(true);
         }
+
+        this.addedPreparedStatements.putAll(results.getAddedPreparedStatements());
+        this.deallocatedPreparedStatements.addAll(results.getDeallocatedPreparedStatements());
 
         currentResults.set(results);
     }