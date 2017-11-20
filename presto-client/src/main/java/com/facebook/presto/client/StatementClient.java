zz22394/presto

Modify CLI to support long prepared statements
In order to support >4k prepared statements, send the prepared
statements along with the query via the Jackson-serialized object
QuerySubmission.

presto-client/src/main/java/com/facebook/presto/client/StatementClient.java
@@ -61,13 +61,13 @@ import static io.airlift.http.client.FullJsonResponseHandler.createFullJsonRespo
 import static io.airlift.http.client.HttpStatus.Family;
 import static io.airlift.http.client.HttpStatus.familyForStatusCode;
 import static io.airlift.http.client.HttpUriBuilder.uriBuilderFrom;
+import static io.airlift.http.client.JsonBodyGenerator.jsonBodyGenerator;
 import static io.airlift.http.client.Request.Builder.prepareDelete;
 import static io.airlift.http.client.Request.Builder.prepareGet;
 import static io.airlift.http.client.Request.Builder.preparePost;
-import static io.airlift.http.client.StaticBodyGenerator.createStaticBodyGenerator;
 import static io.airlift.http.client.StatusResponseHandler.StatusResponse;
 import static io.airlift.http.client.StatusResponseHandler.createStatusResponseHandler;
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static io.airlift.json.JsonCodec.jsonCodec;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
@@ -98,11 +98,18 @@ public class StatementClient
     private final String timeZoneId;
     private final long requestTimeoutNanos;
     private final String user;
+    private final JsonCodec<QuerySubmission> querySubmissionCodec;
 
     public StatementClient(HttpClient httpClient, JsonCodec<QueryResults> queryResultsCodec, ClientSession session, String query)
     {
+        this(httpClient, queryResultsCodec, jsonCodec(QuerySubmission.class), session, query);
+    }
+
+    public StatementClient(HttpClient httpClient, JsonCodec<QueryResults> queryResultsCodec, JsonCodec<QuerySubmission> querySubmissionCodec, ClientSession session, String query)
+    {
         requireNonNull(httpClient, "httpClient is null");
         requireNonNull(queryResultsCodec, "queryResultsCodec is null");
+        requireNonNull(querySubmissionCodec, "querySubmissionCodec is null");
         requireNonNull(session, "session is null");
         requireNonNull(query, "query is null");
 
@@ -113,6 +120,7 @@ public class StatementClient
         this.query = query;
         this.requestTimeoutNanos = session.getClientRequestTimeout().roundTo(NANOSECONDS);
         this.user = session.getUser();
+        this.querySubmissionCodec = querySubmissionCodec;
 
         Request request = buildQueryRequest(session, query);
         JsonResponse<QueryResults> response = httpClient.execute(request, responseHandler);
@@ -127,7 +135,8 @@ public class StatementClient
     private Request buildQueryRequest(ClientSession session, String query)
     {
         Request.Builder builder = prepareRequest(preparePost(), uriBuilderFrom(session.getServer()).replacePath("/v1/statement").build())
-                .setBodyGenerator(createStaticBodyGenerator(query, UTF_8));
+                .setBodyGenerator(jsonBodyGenerator(querySubmissionCodec, new QuerySubmission(query, session.getPreparedStatements())));
+        builder.setHeader(PrestoHeaders.PRESTO_PREPARED_STATEMENT_IN_BODY, "true");
 
         if (session.getSource() != null) {
             builder.setHeader(PrestoHeaders.PRESTO_SOURCE, session.getSource());
@@ -144,11 +153,6 @@ public class StatementClient
         Map<String, String> property = session.getProperties();
         for (Entry<String, String> entry : property.entrySet()) {
             builder.addHeader(PrestoHeaders.PRESTO_SESSION, entry.getKey() + "=" + entry.getValue());
-        }
-
-        Map<String, String> statements = session.getPreparedStatements();
-        for (Entry<String, String> entry : statements.entrySet()) {
-            builder.addHeader(PrestoHeaders.PRESTO_PREPARED_STATEMENT, urlEncode(entry.getKey()) + "=" + urlEncode(entry.getValue()));
         }
 
         builder.setHeader(PrestoHeaders.PRESTO_TRANSACTION_ID, session.getTransactionId() == null ? "NONE" : session.getTransactionId());

starburstdata/facebook-presto

presto-client/src/main/java/com/facebook/presto/client/StatementClient.java
@@ -104,10 +104,17 @@ public class StatementClient
     private final TimeZoneKey timeZone;
     private final long requestTimeoutNanos;
     private final String user;
+    private final JsonCodec<QuerySubmission> querySubmissionCodec;
 
     public StatementClient(OkHttpClient httpClient, ClientSession session, String query)
     {
+        this(httpClient, jsonCodec(QuerySubmission.class), session, query);
+    }
+
+    public StatementClient(OkHttpClient httpClient, JsonCodec<QuerySubmission> querySubmissionCodec, ClientSession session, String query)
+    {
         requireNonNull(httpClient, "httpClient is null");
+        requireNonNull(querySubmissionCodec, "querySubmissionCodec is null");
         requireNonNull(session, "session is null");
         requireNonNull(query, "query is null");
 
@@ -117,6 +124,7 @@ public class StatementClient
         this.query = query;
         this.requestTimeoutNanos = session.getClientRequestTimeout().roundTo(NANOSECONDS);
         this.user = session.getUser();
+        this.querySubmissionCodec = querySubmissionCodec;
 
         Request request = buildQueryRequest(session, query);
 
@@ -137,7 +145,9 @@ public class StatementClient
         url = url.newBuilder().encodedPath("/v1/statement").build();
 
         Request.Builder builder = prepareRequest(url)
-                .post(RequestBody.create(MEDIA_TYPE_JSON, query));
+                .post(RequestBody.create(MEDIA_TYPE_JSON, querySubmissionCodec.toJson(new QuerySubmission(query, session.getPreparedStatements()))));
+
+        builder.addHeader(PrestoHeaders.PRESTO_PREPARED_STATEMENT_IN_BODY, "true");
 
         if (session.getSource() != null) {
             builder.addHeader(PRESTO_SOURCE, session.getSource());
@@ -159,11 +169,6 @@ public class StatementClient
         Map<String, String> property = session.getProperties();
         for (Entry<String, String> entry : property.entrySet()) {
             builder.addHeader(PRESTO_SESSION, entry.getKey() + "=" + entry.getValue());
-        }
-
-        Map<String, String> statements = session.getPreparedStatements();
-        for (Entry<String, String> entry : statements.entrySet()) {
-            builder.addHeader(PRESTO_PREPARED_STATEMENT, urlEncode(entry.getKey()) + "=" + urlEncode(entry.getValue()));
         }
 
         builder.addHeader(PRESTO_TRANSACTION_ID, session.getTransactionId() == null ? "NONE" : session.getTransactionId());