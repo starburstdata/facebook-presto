8fe261ac802bb962e98413f118e26d047484830b
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2017-11-27 12:19:51 +0100

    Add new date time semantics to date time cast operators

--- presto-main/src/test/java/com/facebook/presto/type/TestTimestamp.java
+++ presto-main/src/test/java/com/facebook/presto/type/TestTimestamp.java
@@ -13,11 +13,54 @@
  */
 package com.facebook.presto.type;
 
+import org.testng.annotations.Test;
+
+import static com.facebook.presto.spi.type.TimestampType.TIMESTAMP;
+import static com.facebook.presto.testing.TestingSqlTime.sqlTimestampOf;
+
 public class TestTimestamp
         extends TestTimestampBase
 {
     public TestTimestamp()
     {
         super(false);
+    }
+
+    @Test
+    public void testCastFromSliceContainingTimeZone()
+    {
+        assertFunction(
+                "cast('2001-1-22 03:04:05.321 +07:09' as timestamp)",
+                TIMESTAMP,
+                sqlTimestampOf(2001, 1, 22, 3, 4, 5, 321, DATE_TIME_ZONE, TIME_ZONE_KEY, session));
+        assertFunction(
+                "cast('2001-1-22 03:04:05 +07:09' as timestamp)",
+                TIMESTAMP,
+                sqlTimestampOf(2001, 1, 22, 3, 4, 5, 0, DATE_TIME_ZONE, TIME_ZONE_KEY, session));
+        assertFunction(
+                "cast('2001-1-22 03:04 +07:09' as timestamp)",
+                TIMESTAMP,
+                sqlTimestampOf(2001, 1, 22, 3, 4, 0, 0, DATE_TIME_ZONE, TIME_ZONE_KEY, session));
+        assertFunction(
+                "cast('2001-1-22 +07:09' as timestamp)",
+                TIMESTAMP,
+                sqlTimestampOf(2001, 1, 22, 0, 0, 0, 0, DATE_TIME_ZONE, TIME_ZONE_KEY, session));
+
+        assertFunction(
+                "cast('2001-1-22 03:04:05.321 Asia/Oral' as timestamp)",
+                TIMESTAMP,
+                sqlTimestampOf(2001, 1, 22, 3, 4, 5, 321, DATE_TIME_ZONE, TIME_ZONE_KEY, session));
+        assertFunction(
+                "cast('2001-1-22 03:04:05 Asia/Oral' as timestamp)",
+                TIMESTAMP,
+                sqlTimestampOf(2001, 1, 22, 3, 4, 5, 0, DATE_TIME_ZONE, TIME_ZONE_KEY, session));
+        assertFunction(
+                "cast('2001-1-22 03:04 Asia/Oral' as timestamp)",
+                TIMESTAMP,
+                sqlTimestampOf(2001, 1, 22, 3, 4, 0, 0, DATE_TIME_ZONE, TIME_ZONE_KEY, session));
+        assertFunction(
+                "cast('2001-1-22 Asia/Oral' as timestamp)",
+                TIMESTAMP,
+                sqlTimestampOf(2001, 1, 22, 0, 0, 0, 0, DATE_TIME_ZONE, TIME_ZONE_KEY, session));
     }
 }
