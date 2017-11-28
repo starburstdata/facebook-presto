9d3cedc1cd6dbcc702f6d336104e74531db4ffad
Christina Wallin <christina.wallin@teradata.com>
2017-11-28 14:50:59 +0100

    Add SHOW ROLES to the parser

--- presto-product-tests/src/main/java/com/facebook/presto/tests/hive/TestGrantRevoke.java
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/hive/TestGrantRevoke.java
@@ -24,7 +24,7 @@ import org.testng.annotations.Test;
 import static com.facebook.presto.tests.TestGroups.AUTHORIZATION;
 import static com.facebook.presto.tests.TestGroups.PROFILE_SPECIFIC_TESTS;
 import static com.facebook.presto.tests.utils.QueryExecutors.connectToPresto;
-Simport static com.facebook.presto.tests.utils.QueryExecutors.onHive;
+import static com.facebook.presto.tests.utils.QueryExecutors.onHive;
 import static com.facebook.presto.tests.utils.QueryExecutors.onPresto;
 import static io.prestodb.tempto.assertions.QueryAssert.Row;
 import static io.prestodb.tempto.assertions.QueryAssert.Row.row;
