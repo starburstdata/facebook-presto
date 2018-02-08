7a684e4685afa6fb28115bed73eb56fd4ca939ce
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2018-02-08 10:22:00 +0100

    Support using a fake node count for unit tests
    Allowing the LocalQueryRunner to estimate costs using a fake node count
    allows unit tests to consider network costs and different cluster
    configurations.

--- presto-main/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
+++ presto-main/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
@@ -287,6 +287,11 @@ public class LocalQueryRunner
 
     public LocalQueryRunner(Session defaultSession, FeaturesConfig featuresConfig, NodeSpillConfig nodeSpillConfig, boolean withInitialTransaction, boolean alwaysRevokeMemory)
     {
+        this(defaultSession, featuresConfig, nodeSpillConfig, withInitialTransaction, alwaysRevokeMemory, 1);
+    }
+
+    private LocalQueryRunner(Session defaultSession, FeaturesConfig featuresConfig, NodeSpillConfig nodeSpillConfig, boolean withInitialTransaction, boolean alwaysRevokeMemory, int nodeCountForStats)
+    {
         requireNonNull(defaultSession, "defaultSession is null");
         checkArgument(!defaultSession.getTransactionId().isPresent() || !withInitialTransaction, "Already in transaction");
 
@@ -331,7 +336,7 @@ public class LocalQueryRunner
                 new CoefficientBasedStatsCalculator(metadata),
                 ServerMainModule.createNewStatsCalculator(metadata, new ScalarStatsCalculator(metadata), new SemiJoinStatsCalculator()));
         this.costCalculator = new CostCalculatorUsingExchanges(this::getNodeCount);
-        this.estimatedExchangesCostCalculator = new CostCalculatorWithEstimatedExchanges(costCalculator, this::getNodeCount);
+        this.estimatedExchangesCostCalculator = new CostCalculatorWithEstimatedExchanges(costCalculator, () -> nodeCountForStats);
         this.accessControl = new TestingAccessControlManager(transactionManager);
         this.pageSourceManager = new PageSourceManager();
 
@@ -418,6 +423,11 @@ public class LocalQueryRunner
     {
         checkArgument(!defaultSession.getTransactionId().isPresent(), "Already in transaction!");
         return new LocalQueryRunner(defaultSession, new FeaturesConfig(), new NodeSpillConfig(), true, false);
+    }
+
+    public static LocalQueryRunner queryRunnerWithFakeNodeCountForStats(Session defaultSession, int nodeCount)
+    {
+        return new LocalQueryRunner(defaultSession, new FeaturesConfig(), new NodeSpillConfig(), false, false, nodeCount);
     }
 
     @Override
