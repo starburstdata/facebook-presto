d68d0dd5019dc96bf29b350bcbe3cf8a902bc6bd
Karol Sobczak <Karol.Sobczak@Teradata.com>
2018-02-08 10:21:48 +0100

    Handle symbol = symbol explicitly in stats

--- presto-main/src/main/java/com/facebook/presto/cost/FilterStatsCalculator.java
+++ presto-main/src/main/java/com/facebook/presto/cost/FilterStatsCalculator.java
@@ -295,6 +295,10 @@ public class FilterStatsCalculator
                 return visitExpression(node, context);
             }
 
+            if (left instanceof SymbolReference && right instanceof SymbolReference && left.equals(right)) {
+                return process(new IsNotNullPredicate(left));
+            }
+
             return comparisonExpressionToExpressionStats(input, leftSymbol, leftStats.get(), rightSymbol, rightStats.get(), type);
         }
 
