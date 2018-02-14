03843fe4be1bd730c80d556553628f1c945efc53
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-02-14 11:53:58 +0100

    Fix order of arguments to assertEquals

--- presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/TestMemo.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/TestMemo.java
@@ -220,13 +220,13 @@ public class TestMemo
         memo.storeStats(yGroup, yStats);
         memo.storeStats(xGroup, xStats);
 
-        assertEquals(Optional.of(yStats), memo.getStats(yGroup));
-        assertEquals(Optional.of(xStats), memo.getStats(xGroup));
+        assertEquals(memo.getStats(yGroup), Optional.of(yStats));
+        assertEquals(memo.getStats(xGroup), Optional.of(xStats));
 
         memo.replace(yGroup, node(), "rule");
 
-        assertEquals(Optional.empty(), memo.getStats(yGroup));
-        assertEquals(Optional.empty(), memo.getStats(xGroup));
+        assertEquals(memo.getStats(yGroup), Optional.empty());
+        assertEquals(memo.getStats(xGroup), Optional.empty());
     }
 
     @Test
