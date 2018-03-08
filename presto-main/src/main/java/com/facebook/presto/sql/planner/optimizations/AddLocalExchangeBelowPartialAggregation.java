61ffccb1bea493355744d2e9349a515a79ddd1c2
Karol Sobczak <karol.sobczak@karolsobczak.com>
2018-03-08 11:45:20 +0100

    Add rule for adding hash local exchange below partial aggregation

---
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/AddLocalExchangeBelowPartialAggregation.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.planner.optimizations;
+
+import com.facebook.presto.matching.Captures;
+import com.facebook.presto.matching.Pattern;
+import com.facebook.presto.metadata.Metadata;
+import com.facebook.presto.sql.parser.SqlParser;
+import com.facebook.presto.sql.planner.iterative.Rule;
+import com.facebook.presto.sql.planner.optimizations.StreamPropertyDerivations.StreamProperties;
+import com.facebook.presto.sql.planner.plan.AggregationNode;
+import com.facebook.presto.sql.planner.plan.PlanNode;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.facebook.presto.sql.planner.optimizations.StreamPreferredProperties.fixedParallelism;
+import static com.facebook.presto.sql.planner.optimizations.StreamPropertyDerivations.deriveProperties;
+import static com.facebook.presto.sql.planner.plan.ExchangeNode.Scope.LOCAL;
+import static com.facebook.presto.sql.planner.plan.ExchangeNode.partitionedExchange;
+import static com.facebook.presto.sql.planner.plan.Patterns.Aggregation.groupingKeys;
+import static com.facebook.presto.sql.planner.plan.Patterns.Aggregation.step;
+import static com.facebook.presto.sql.planner.plan.Patterns.aggregation;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.util.Objects.requireNonNull;
+
+public class AddLocalExchangeBelowPartialAggregation
+        implements Rule<AggregationNode>
+{
+    private static final Pattern<AggregationNode> PATTERN = aggregation()
+            .with(step().equalTo(AggregationNode.Step.PARTIAL))
+            .with(Pattern.nonEmpty(groupingKeys()));
+
+    private final Metadata metadata;
+    private final SqlParser parser;
+
+    public AddLocalExchangeBelowPartialAggregation(Metadata metadata, SqlParser parser)
+    {
+        this.metadata = requireNonNull(metadata, "metadata is null");
+        this.parser = requireNonNull(parser, "parser is null");
+    }
+
+    @Override
+    public Pattern<AggregationNode> getPattern()
+    {
+        return PATTERN;
+    }
+
+    @Override
+    public Result apply(AggregationNode node, Captures captures, Context context)
+    {
+        StreamProperties childProperties = derivePropertiesRecursively(node.getSource(), context);
+        StreamPreferredProperties requiredProperties = fixedParallelism().withPartitioning(node.getGroupingKeys());
+
+        if (requiredProperties.isSatisfiedBy(childProperties)) {
+            return Result.empty();
+        }
+
+        return Result.ofPlanNode(
+                node.replaceChildren(ImmutableList.of(
+                        partitionedExchange(
+                                context.getIdAllocator().getNextId(),
+                                LOCAL,
+                                node.getSource(),
+                                node.getGroupingKeys(),
+                                Optional.empty()))));
+    }
+
+    private StreamProperties derivePropertiesRecursively(PlanNode node, Context context)
+    {
+        PlanNode resolvedPlanNode = context.getLookup().resolve(node);
+        List<StreamProperties> inputProperties = resolvedPlanNode.getSources().stream()
+                .map(source -> derivePropertiesRecursively(source, context))
+                .collect(toImmutableList());
+        return deriveProperties(resolvedPlanNode, inputProperties, metadata, context.getSession(), context.getSymbolAllocator().getTypes(), parser);
+    }
+}
---
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/TestAddLocalExchangeBelowPartialAggregation.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.planner.iterative.rule;
+
+import com.facebook.presto.sql.parser.SqlParser;
+import com.facebook.presto.sql.planner.Symbol;
+import com.facebook.presto.sql.planner.iterative.rule.test.BaseRuleTest;
+import com.facebook.presto.sql.planner.optimizations.AddLocalExchangeBelowPartialAggregation;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+import static com.facebook.presto.spi.type.DoubleType.DOUBLE;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.aggregation;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.exchange;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.functionCall;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.values;
+import static com.facebook.presto.sql.planner.iterative.rule.test.PlanBuilder.expression;
+import static com.facebook.presto.sql.planner.plan.AggregationNode.Step.FINAL;
+import static com.facebook.presto.sql.planner.plan.AggregationNode.Step.PARTIAL;
+import static com.facebook.presto.sql.planner.plan.ExchangeNode.Scope.LOCAL;
+import static com.facebook.presto.sql.planner.plan.ExchangeNode.Type.REPARTITION;
+
+public class TestAddLocalExchangeBelowPartialAggregation
+        extends BaseRuleTest
+{
+    private static final SqlParser SQL_PARSER = new SqlParser();
+
+    @Test
+    public void testDoesNotFireForFinalAggregation()
+    {
+        tester().assertThat(new AddLocalExchangeBelowPartialAggregation(tester().getMetadata(), SQL_PARSER))
+                .on(p -> p.aggregation(ab -> ab
+                        .source(p.values(p.symbol("GROUP_COL"), p.symbol("AGGR_COL")))
+                        .addAggregation(p.symbol("AVG", DOUBLE), expression("AVG(AGGR_COL)"), ImmutableList.of(DOUBLE))
+                        .addGroupingSet(p.symbol("GROUP_COL"))
+                        .step(FINAL)))
+                .doesNotFire();
+    }
+
+    @Test
+    public void testDoesAddLocalHashExchangeBelowPartialAggregation()
+    {
+        tester().assertThat(new AddLocalExchangeBelowPartialAggregation(tester().getMetadata(), SQL_PARSER))
+                .on(p -> p.aggregation(ab -> ab
+                        .source(p.values(p.symbol("GROUP_COL"), p.symbol("AGGR_COL")))
+                        .addAggregation(p.symbol("AVG", DOUBLE), expression("AVG(AGGR_COL)"), ImmutableList.of(DOUBLE))
+                        .addGroupingSet(p.symbol("GROUP_COL"))
+                        .step(PARTIAL)))
+                .matches(
+                        aggregation(
+                                ImmutableList.of(ImmutableList.of("GROUP_COL")),
+                                ImmutableMap.of(Optional.of("AVG"), functionCall("avg", ImmutableList.of("AGGR_COL"))),
+                                ImmutableMap.of(),
+                                Optional.empty(),
+                                PARTIAL,
+                                exchange(LOCAL, REPARTITION,
+                                        values("GROUP_COL", "AGGR_COL"))));
+    }
+
+    @Test
+    public void testDoesNotFireWhenAlreadyPartitioned()
+    {
+        tester().assertThat(new AddLocalExchangeBelowPartialAggregation(tester().getMetadata(), SQL_PARSER))
+                .on(p -> {
+                    Symbol groupSymbol1 = p.symbol("GROUP_COL1");
+                    Symbol groupSymbol2 = p.symbol("GROUP_COL2");
+                    Symbol aggregationSymol = p.symbol("AGGR_COL");
+                    Symbol averageSymbol = p.symbol("AVG", DOUBLE);
+                    return p.aggregation(ab -> ab
+                            .source(p.exchange(e -> e
+                                    .addSource(p.values(groupSymbol1, groupSymbol2, aggregationSymol))
+                                    .addInputsSet(groupSymbol1, groupSymbol2, aggregationSymol)
+                                    .type(REPARTITION)
+                                    .scope(LOCAL)
+                                    .fixedHashDistributionParitioningScheme(
+                                            ImmutableList.of(groupSymbol1, groupSymbol2, aggregationSymol),
+                                            ImmutableList.of(groupSymbol1))))
+                            .addAggregation(averageSymbol, expression("AVG(AGGR_COL)"), ImmutableList.of(DOUBLE))
+                            .addGroupingSet(groupSymbol1, groupSymbol2)
+                            .step(PARTIAL));
+                })
+                .doesNotFire();
+    }
+}
