9aeb7919fe57518b9e42c1ed2f67586b2506f6d9
Karol Sobczak <karol.sobczak@karolsobczak.com>
2018-03-08 11:54:43 +0100

    Add AddLocalExchangeBelowPartialAggregation rule to plan optimizers

--- presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizers.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizers.java
@@ -86,6 +86,7 @@ import com.facebook.presto.sql.planner.iterative.rule.TransformSpatialPredicateT
 import com.facebook.presto.sql.planner.iterative.rule.TransformUncorrelatedInPredicateSubqueryToSemiJoin;
 import com.facebook.presto.sql.planner.iterative.rule.TransformUncorrelatedLateralToJoin;
 import com.facebook.presto.sql.planner.optimizations.AddExchanges;
+import com.facebook.presto.sql.planner.optimizations.AddLocalExchangeBelowPartialAggregation;
 import com.facebook.presto.sql.planner.optimizations.AddLocalExchanges;
 import com.facebook.presto.sql.planner.optimizations.BeginTableWrite;
 import com.facebook.presto.sql.planner.optimizations.CheckSubqueryNodesAreRewritten;
@@ -425,6 +426,11 @@ public class PlanOptimizers
                         new PushPartialAggregationThroughJoin(),
                         new PushPartialAggregationThroughExchange(metadata.getFunctionRegistry()),
                         new PruneJoinColumns())));
+        builder.add(new IterativeOptimizer(
+                stats,
+                statsCalculator,
+                costCalculator,
+                ImmutableSet.of(new AddLocalExchangeBelowPartialAggregation(metadata, sqlParser))));
         builder.add(new IterativeOptimizer(
                 stats,
                 statsCalculator,
