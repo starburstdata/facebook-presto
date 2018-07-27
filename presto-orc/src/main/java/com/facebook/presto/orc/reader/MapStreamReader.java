prestodb/presto

Pass row count explicitly to MapBlock constructor

presto-orc/src/main/java/com/facebook/presto/orc/reader/MapStreamReader.java
@@ -155,10 +155,12 @@ public class MapStreamReader
             offsets[i] = offsets[i - 1] + length;
         }
 
+        Block result = mapType.createBlockFromKeyValue(nextBatchSize, nullVector, offsets, keyValueBlock[0], keyValueBlock[1]);
+
         readOffset = 0;
         nextBatchSize = 0;
 
-        return mapType.createBlockFromKeyValue(nullVector, offsets, keyValueBlock[0], keyValueBlock[1]);
+        return result;
     }
 
     private static Block[] createKeyValueBlock(int positionCount, Block keys, Block values, int[] lengths)

presto-spi/src/main/java/com/facebook/presto/spi/type/MapType.java
@@ -234,9 +234,10 @@ public class MapType
         return "map(" + keyType.getDisplayName() + ", " + valueType.getDisplayName() + ")";
     }
 
-    public MapBlock createBlockFromKeyValue(boolean[] mapIsNull, int[] offsets, Block keyBlock, Block valueBlock)
+    public MapBlock createBlockFromKeyValue(int rowCount, boolean[] mapIsNull, int[] offsets, Block keyBlock, Block valueBlock)
     {
         return MapBlock.fromKeyValueBlock(
+                rowCount,
                 mapIsNull,
                 offsets,
                 keyBlock,

starburstdata/facebook-presto

presto-orc/src/main/java/com/facebook/presto/orc/reader/MapStreamReader.java
@@ -156,10 +156,10 @@ public class MapStreamReader
             currentLength = lastLength;
         }
 
+        Block result = mapType.createBlockFromKeyValue(nextBatchSize, nullVector, offsetVector, keyValueBlock[0], keyValueBlock[1]);
         readOffset = 0;
         nextBatchSize = 0;
-
-        return mapType.createBlockFromKeyValue(nullVector, offsetVector, keyValueBlock[0], keyValueBlock[1]);
+        return result;
     }
 
     private static Block[] createKeyValueBlock(int positionCount, Block keys, Block values, int[] lengths)

presto-spi/src/main/java/com/facebook/presto/spi/type/MapType.java
@@ -250,9 +250,10 @@ public class MapType
         return "map(" + keyType.getDisplayName() + ", " + valueType.getDisplayName() + ")";
     }
 
-    public Block createBlockFromKeyValue(boolean[] mapIsNull, int[] offsets, Block keyBlock, Block valueBlock)
+    public Block createBlockFromKeyValue(int rowCount, boolean[] mapIsNull, int[] offsets, Block keyBlock, Block valueBlock)
     {
         return MapBlock.fromKeyValueBlock(
+                rowCount,
                 mapIsNull,
                 offsets,
                 keyBlock,