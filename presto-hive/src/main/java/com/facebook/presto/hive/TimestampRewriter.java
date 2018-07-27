prestodb/presto

Add support for MAP type in TimestampRewriter

presto-hive/src/main/java/com/facebook/presto/hive/TimestampRewriter.java
@@ -15,13 +15,16 @@ package com.facebook.presto.hive;
 
 import com.facebook.presto.spi.Page;
 import com.facebook.presto.spi.block.AbstractArrayBlock;
+import com.facebook.presto.spi.block.AbstractMapBlock;
 import com.facebook.presto.spi.block.ArrayBlock;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockBuilderStatus;
 import com.facebook.presto.spi.block.LazyBlock;
 import com.facebook.presto.spi.type.ArrayType;
+import com.facebook.presto.spi.type.MapType;
 import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.spi.type.TypeManager;
 import org.joda.time.DateTimeZone;
 
 import java.util.List;
@@ -34,11 +37,13 @@ public class TimestampRewriter
 {
     private final List<Type> columnTypes;
     private final DateTimeZone storageTimeZone;
+    private final TypeManager typeManager;
 
-    public TimestampRewriter(List<Type> columnTypes, DateTimeZone storageTimeZone)
+    public TimestampRewriter(List<Type> columnTypes, DateTimeZone storageTimeZone, TypeManager typeManager)
     {
         this.columnTypes = columnTypes;
         this.storageTimeZone = storageTimeZone;
+        this.typeManager = typeManager;
     }
 
     public Page rewritePageHiveToPresto(Page page)
@@ -104,6 +109,9 @@ public class TimestampRewriter
         if (type instanceof ArrayType) {
             return modifyTimestampsInArrayBlock(type, block, modification);
         }
+        if (type instanceof MapType) {
+            return modifyTimestampsInMapBlock(type, block, modification);
+        }
         throw new IllegalArgumentException("Unsupported block; block=" + block.getClass().getName() + "; type=" + type);
     }
 
@@ -155,5 +163,21 @@ public class TimestampRewriter
             }
             return builder.build();
         }
+    }
+
+    private Block modifyTimestampsInMapBlock(Type type, Block block, LongUnaryOperator modification)
+    {
+        if (block instanceof AbstractMapBlock) {
+            AbstractMapBlock mapBlock = (AbstractMapBlock) block;
+            MapType mapType = (MapType) type;
+            Type keyType = type.getTypeParameters().get(0);
+            Type valueType = type.getTypeParameters().get(1);
+            Block innerKeyBlock = modifyTimestampsInBlock(mapBlock.getKeys(), keyType, modification);
+            Block innerValueBlock = modifyTimestampsInBlock(mapBlock.getValues(), valueType, modification);
+
+            return mapType.createBlockFromKeyValue(mapBlock.getPositionCount(), mapBlock.getMapIsNull(), mapBlock.getOffsets(), innerKeyBlock, innerValueBlock);
+        }
+
+        throw new IllegalArgumentException("Not supported block type; blockType=" + block.getClass().getName() + "; prestoType=" + type);
     }
 }

presto-spi/src/main/java/com/facebook/presto/spi/block/AbstractMapBlock.java
@@ -30,7 +30,7 @@ public abstract class AbstractMapBlock
         implements Block
 {
     // inverse of hash fill ratio, must be integer
-    static final int HASH_MULTIPLIER = 2;
+    public static final int HASH_MULTIPLIER = 2;
 
     protected final Type keyType;
     protected final MethodHandle keyNativeHashCode;
@@ -45,25 +45,25 @@ public abstract class AbstractMapBlock
         this.keyBlockNativeEquals = keyBlockNativeEquals;
     }
 
-    protected abstract Block getKeys();
+    public abstract Block getKeys();
 
-    protected abstract Block getValues();
+    public abstract Block getValues();
 
-    protected abstract int[] getHashTables();
+    public abstract int[] getHashTables();
 
     /**
      * offset is entry-based, not position-based. In other words,
      * if offset[1] is 6, it means the first map has 6 key-value pairs,
      * not 6 key/values (which would be 3 pairs).
      */
-    protected abstract int[] getOffsets();
+    public abstract int[] getOffsets();
 
     /**
      * offset is entry-based, not position-based. (see getOffsets)
      */
-    protected abstract int getOffsetBase();
+    public abstract int getOffsetBase();
 
-    protected abstract boolean[] getMapIsNull();
+    public abstract boolean[] getMapIsNull();
 
     int getOffset(int position)
     {

presto-spi/src/main/java/com/facebook/presto/spi/block/MapBlock.java
@@ -81,37 +81,37 @@ public class MapBlock
     }
 
     @Override
-    protected Block getKeys()
+    public Block getKeys()
     {
         return keyBlock;
     }
 
     @Override
-    protected Block getValues()
+    public Block getValues()
     {
         return valueBlock;
     }
 
     @Override
-    protected int[] getHashTables()
+    public int[] getHashTables()
     {
         return hashTables;
     }
 
     @Override
-    protected int[] getOffsets()
+    public int[] getOffsets()
     {
         return offsets;
     }
 
     @Override
-    protected int getOffsetBase()
+    public int getOffsetBase()
     {
         return startOffset;
     }
 
     @Override
-    protected boolean[] getMapIsNull()
+    public boolean[] getMapIsNull()
     {
         return mapIsNull;
     }

presto-spi/src/main/java/com/facebook/presto/spi/block/MapBlockBuilder.java
@@ -97,37 +97,37 @@ public class MapBlockBuilder
     }
 
     @Override
-    protected Block getKeys()
+    public Block getKeys()
     {
         return keyBlockBuilder;
     }
 
     @Override
-    protected Block getValues()
+    public Block getValues()
     {
         return valueBlockBuilder;
     }
 
     @Override
-    protected int[] getHashTables()
+    public int[] getHashTables()
     {
         return hashTables;
     }
 
     @Override
-    protected int[] getOffsets()
+    public int[] getOffsets()
     {
         return offsets;
     }
 
     @Override
-    protected int getOffsetBase()
+    public int getOffsetBase()
     {
         return 0;
     }
 
     @Override
-    protected boolean[] getMapIsNull()
+    public boolean[] getMapIsNull()
     {
         return mapIsNull;
     }
@@ -309,14 +309,14 @@ public class MapBlockBuilder
                 newNegativeOneFilledArray(newSize * HASH_MULTIPLIER));
     }
 
-    private static int[] newNegativeOneFilledArray(int size)
+    public static int[] newNegativeOneFilledArray(int size)
     {
         int[] hashTable = new int[size];
         Arrays.fill(hashTable, -1);
         return hashTable;
     }
 
-    static void buildHashTable(Block keyBlock, int keyOffset, int keyCount, MethodHandle keyBlockHashCode, int[] outputHashTable, int hashTableOffset, int hashTableSize)
+    public static void buildHashTable(Block keyBlock, int keyOffset, int keyCount, MethodHandle keyBlockHashCode, int[] outputHashTable, int hashTableOffset, int hashTableSize)
     {
         // This method assumes that keyBlock has no duplicated entries (in the specified range)
         for (int i = 0; i < keyCount; i++) {

starburstdata/facebook-presto

presto-hive/src/main/java/com/facebook/presto/hive/TimestampRewriter.java
@@ -15,13 +15,16 @@ package com.facebook.presto.hive;
 
 import com.facebook.presto.spi.Page;
 import com.facebook.presto.spi.block.AbstractArrayBlock;
+import com.facebook.presto.spi.block.AbstractMapBlock;
 import com.facebook.presto.spi.block.ArrayBlock;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.LazyBlock;
 import com.facebook.presto.spi.block.PageBuilderStatus;
 import com.facebook.presto.spi.type.ArrayType;
+import com.facebook.presto.spi.type.MapType;
 import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.spi.type.TypeManager;
 import org.joda.time.DateTimeZone;
 
 import java.util.List;
@@ -34,11 +37,13 @@ public class TimestampRewriter
 {
     private final List<Type> columnTypes;
     private final DateTimeZone storageTimeZone;
+    private final TypeManager typeManager;
 
-    public TimestampRewriter(List<Type> columnTypes, DateTimeZone storageTimeZone)
+    public TimestampRewriter(List<Type> columnTypes, DateTimeZone storageTimeZone, TypeManager typeManager)
     {
         this.columnTypes = columnTypes;
         this.storageTimeZone = storageTimeZone;
+        this.typeManager = typeManager;
     }
 
     public Page rewritePageHiveToPresto(Page page)
@@ -102,6 +107,9 @@ public class TimestampRewriter
         if (type instanceof ArrayType) {
             return modifyTimestampsInArrayBlock(type, block, modification);
         }
+        if (type instanceof MapType) {
+            return modifyTimestampsInMapBlock(type, block, modification);
+        }
         throw new IllegalArgumentException("Unsupported block; block=" + block.getClass().getName() + "; type=" + type);
     }
 
@@ -153,5 +161,21 @@ public class TimestampRewriter
             }
             return builder.build();
         }
+    }
+
+    private Block modifyTimestampsInMapBlock(Type type, Block block, LongUnaryOperator modification)
+    {
+        if (block instanceof AbstractMapBlock) {
+            AbstractMapBlock mapBlock = (AbstractMapBlock) block;
+            MapType mapType = (MapType) type;
+            Type keyType = type.getTypeParameters().get(0);
+            Type valueType = type.getTypeParameters().get(1);
+            Block innerKeyBlock = modifyTimestampsInBlock(mapBlock.getRawKeyBlock(), keyType, modification);
+            Block innerValueBlock = modifyTimestampsInBlock(mapBlock.getRawValueBlock(), valueType, modification);
+
+            return mapType.createBlockFromKeyValue(mapBlock.getPositionCount(), mapBlock.getMapIsNull(), mapBlock.getOffsets(), innerKeyBlock, innerValueBlock);
+        }
+
+        throw new IllegalArgumentException("Not supported block type; blockType=" + block.getClass().getName() + "; prestoType=" + type);
     }
 }

presto-spi/src/main/java/com/facebook/presto/spi/block/AbstractMapBlock.java
@@ -30,7 +30,7 @@ public abstract class AbstractMapBlock
         implements Block
 {
     // inverse of hash fill ratio, must be integer
-    static final int HASH_MULTIPLIER = 2;
+    public static final int HASH_MULTIPLIER = 2;
 
     protected final Type keyType;
     protected final MethodHandle keyNativeHashCode;
@@ -45,25 +45,25 @@ public abstract class AbstractMapBlock
         this.keyBlockNativeEquals = keyBlockNativeEquals;
     }
 
-    protected abstract Block getRawKeyBlock();
+    public abstract Block getRawKeyBlock();
 
-    protected abstract Block getRawValueBlock();
+    public abstract Block getRawValueBlock();
 
-    protected abstract int[] getHashTables();
+    public abstract int[] getHashTables();
 
     /**
      * offset is entry-based, not position-based. In other words,
      * if offset[1] is 6, it means the first map has 6 key-value pairs,
      * not 6 key/values (which would be 3 pairs).
      */
-    protected abstract int[] getOffsets();
+    public abstract int[] getOffsets();
 
     /**
      * offset is entry-based, not position-based. (see getOffsets)
      */
-    protected abstract int getOffsetBase();
+    public abstract int getOffsetBase();
 
-    protected abstract boolean[] getMapIsNull();
+    public abstract boolean[] getMapIsNull();
 
     int getOffset(int position)
     {

presto-spi/src/main/java/com/facebook/presto/spi/block/MapBlock.java
@@ -194,37 +194,37 @@ public class MapBlock
     }
 
     @Override
-    protected Block getRawKeyBlock()
+    public Block getRawKeyBlock()
     {
         return keyBlock;
     }
 
     @Override
-    protected Block getRawValueBlock()
+    public Block getRawValueBlock()
     {
         return valueBlock;
     }
 
     @Override
-    protected int[] getHashTables()
+    public int[] getHashTables()
     {
         return hashTables;
     }
 
     @Override
-    protected int[] getOffsets()
+    public int[] getOffsets()
     {
         return offsets;
     }
 
     @Override
-    protected int getOffsetBase()
+    public int getOffsetBase()
     {
         return startOffset;
     }
 
     @Override
-    protected boolean[] getMapIsNull()
+    public boolean[] getMapIsNull()
     {
         return mapIsNull;
     }

presto-spi/src/main/java/com/facebook/presto/spi/block/MapBlockBuilder.java
@@ -102,37 +102,37 @@ public class MapBlockBuilder
     }
 
     @Override
-    protected Block getRawKeyBlock()
+    public Block getRawKeyBlock()
     {
         return keyBlockBuilder;
     }
 
     @Override
-    protected Block getRawValueBlock()
+    public Block getRawValueBlock()
     {
         return valueBlockBuilder;
     }
 
     @Override
-    protected int[] getHashTables()
+    public int[] getHashTables()
     {
         return hashTables;
     }
 
     @Override
-    protected int[] getOffsets()
+    public int[] getOffsets()
     {
         return offsets;
     }
 
     @Override
-    protected int getOffsetBase()
+    public int getOffsetBase()
     {
         return 0;
     }
 
     @Override
-    protected boolean[] getMapIsNull()
+    public boolean[] getMapIsNull()
     {
         return mapIsNull;
     }
@@ -407,7 +407,7 @@ public class MapBlockBuilder
                 newNegativeOneFilledArray(newSize * HASH_MULTIPLIER));
     }
 
-    private static int[] newNegativeOneFilledArray(int size)
+    public static int[] newNegativeOneFilledArray(int size)
     {
         int[] hashTable = new int[size];
         Arrays.fill(hashTable, -1);
@@ -417,7 +417,7 @@ public class MapBlockBuilder
     /**
      * This method assumes that {@code keyBlock} has no duplicated entries (in the specified range)
      */
-    static void buildHashTable(Block keyBlock, int keyOffset, int keyCount, MethodHandle keyBlockHashCode, int[] outputHashTable, int hashTableOffset, int hashTableSize)
+    public static void buildHashTable(Block keyBlock, int keyOffset, int keyCount, MethodHandle keyBlockHashCode, int[] outputHashTable, int hashTableOffset, int hashTableSize)
     {
         for (int i = 0; i < keyCount; i++) {
             int hash = getHashPosition(keyBlock, keyOffset + i, keyBlockHashCode, hashTableSize);