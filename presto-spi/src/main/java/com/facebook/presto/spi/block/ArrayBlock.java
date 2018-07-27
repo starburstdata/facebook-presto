prestodb/presto

Add support for ARRAY type in TimestampRewriter

presto-spi/src/main/java/com/facebook/presto/spi/block/ArrayBlock.java
@@ -107,25 +107,25 @@ public class ArrayBlock
     }
 
     @Override
-    protected Block getValues()
+    public Block getValues()
     {
         return values;
     }
 
     @Override
-    protected int[] getOffsets()
+    public int[] getOffsets()
     {
         return offsets;
     }
 
     @Override
-    protected int getOffsetBase()
+    public int getOffsetBase()
     {
         return arrayOffset;
     }
 
     @Override
-    protected boolean[] getValueIsNull()
+    public boolean[] getValueIsNull()
     {
         return valueIsNull;
     }

starburstdata/facebook-presto

presto-spi/src/main/java/com/facebook/presto/spi/block/ArrayBlock.java
@@ -143,25 +143,25 @@ public class ArrayBlock
     }
 
     @Override
-    protected Block getRawElementBlock()
+    public Block getRawElementBlock()
     {
         return values;
     }
 
     @Override
-    protected int[] getOffsets()
+    public int[] getOffsets()
     {
         return offsets;
     }
 
     @Override
-    protected int getOffsetBase()
+    public int getOffsetBase()
     {
         return arrayOffset;
     }
 
     @Override
-    protected boolean[] getValueIsNull()
+    public boolean[] getValueIsNull()
     {
         return valueIsNull;
     }