prestodb/presto

Add support for ROW type in TimestampRewriter

presto-spi/src/main/java/com/facebook/presto/spi/block/RowBlock.java
@@ -61,25 +61,25 @@ public class RowBlock
     }
 
     @Override
-    protected Block[] getFieldBlocks()
+    public Block[] getFieldBlocks()
     {
         return fieldBlocks;
     }
 
     @Override
-    protected int[] getFieldBlockOffsets()
+    public int[] getFieldBlockOffsets()
     {
         return fieldBlockOffsets;
     }
 
     @Override
-    protected int getOffsetBase()
+    public int getOffsetBase()
     {
         return startOffset;
     }
 
     @Override
-    protected boolean[] getRowIsNull()
+    public boolean[] getRowIsNull()
     {
         return rowIsNull;
     }

starburstdata/facebook-presto

presto-spi/src/main/java/com/facebook/presto/spi/block/RowBlock.java
@@ -53,7 +53,7 @@ public class RowBlock
     /**
      * Create a row block directly without per element validations.
      */
-    static RowBlock createRowBlockInternal(int startOffset, int positionCount, boolean[] rowIsNull, int[] fieldBlockOffsets, Block[] fieldBlocks)
+    public static RowBlock createRowBlockInternal(int startOffset, int positionCount, boolean[] rowIsNull, int[] fieldBlockOffsets, Block[] fieldBlocks)
     {
         validateConstructorArguments(startOffset, positionCount, rowIsNull, fieldBlockOffsets, fieldBlocks);
         return new RowBlock(startOffset, positionCount, rowIsNull, fieldBlockOffsets, fieldBlocks);
@@ -116,25 +116,25 @@ public class RowBlock
     }
 
     @Override
-    protected Block[] getRawFieldBlocks()
+    public Block[] getRawFieldBlocks()
     {
         return fieldBlocks;
     }
 
     @Override
-    protected int[] getFieldBlockOffsets()
+    public int[] getFieldBlockOffsets()
     {
         return fieldBlockOffsets;
     }
 
     @Override
-    protected int getOffsetBase()
+    public int getOffsetBase()
     {
         return startOffset;
     }
 
     @Override
-    protected boolean[] getRowIsNull()
+    public boolean[] getRowIsNull()
     {
         return rowIsNull;
     }