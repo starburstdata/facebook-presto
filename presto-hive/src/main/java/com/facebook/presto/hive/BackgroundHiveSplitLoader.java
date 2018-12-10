zhdk123456/presto

Allow empty partitions for clustered hive tables

presto-hive/src/main/java/com/facebook/presto/hive/BackgroundHiveSplitLoader.java
@@ -71,6 +71,7 @@ import static com.facebook.presto.hive.HiveErrorCode.HIVE_INVALID_METADATA;
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_INVALID_PARTITION_VALUE;
 import static com.facebook.presto.hive.HiveSessionProperties.getMaxInitialSplitSize;
 import static com.facebook.presto.hive.HiveSessionProperties.getMaxSplitSize;
+import static com.facebook.presto.hive.HiveSessionProperties.isEmptyBucketedPartitionsEnabled;
 import static com.facebook.presto.hive.HiveSessionProperties.isMultiFileBucketingEnabled;
 import static com.facebook.presto.hive.HiveUtil.checkCondition;
 import static com.facebook.presto.hive.HiveUtil.getInputFormat;
@@ -79,6 +80,7 @@ import static com.facebook.presto.hive.UnpartitionedPartition.isUnpartitioned;
 import static com.facebook.presto.spi.StandardErrorCode.NOT_SUPPORTED;
 import static com.google.common.base.Preconditions.checkState;
 import static java.lang.String.format;
+import static java.util.Collections.emptySet;
 import static org.apache.hadoop.hive.common.FileUtils.HIDDEN_FILES_PATH_FILTER;
 
 public class BackgroundHiveSplitLoader
@@ -402,6 +404,13 @@ public class BackgroundHiveSplitLoader
         List<Set<LocatedFileStatus>> bucketList = ImmutableList.of();
         if (fileStatuses.size() == bucketCount) {
             bucketList = getBucketFilesSetsSingle(fileStatuses);
+        }
+        else if (fileStatuses.size() == 0 && isEmptyBucketedPartitionsEnabled(session)) {
+            ImmutableList.Builder<Set<LocatedFileStatus>> emptyBuckets = ImmutableList.builder();
+            for (int i = 0; i < bucketCount; ++i) {
+                emptyBuckets.add(emptySet());
+            }
+            bucketList = emptyBuckets.build();
         }
         else if (isMultiFileBucketingEnabled(session)) {
             List<Set<LocatedFileStatus>> multiFileBucketList = getBucketFilesSetsMulti(fileStatuses);

presto-hive/src/main/java/com/facebook/presto/hive/HiveSessionProperties.java
@@ -37,6 +37,7 @@ public final class HiveSessionProperties
     private static final String MAX_SPLIT_SIZE = "max_split_size";
     private static final String MAX_INITIAL_SPLIT_SIZE = "max_initial_split_size";
     private static final String MULTI_FILE_BUCKETING_ENABLED = "multi_file_bucketing_enabled";
+    private static final String EMPTY_BUCKETED_PARTITIONS_ENABLED = "empty_bucketed_partitions_enabled";
 
     private final List<PropertyMetadata<?>> sessionProperties;
 
@@ -88,6 +89,11 @@ public final class HiveSessionProperties
                         MULTI_FILE_BUCKETING_ENABLED,
                         "Allow multiple files per bucket for clustered table",
                         config.isMultiFileBucketingEnabled(),
+                        true),
+                booleanSessionProperty(
+                        EMPTY_BUCKETED_PARTITIONS_ENABLED,
+                        "Allow partitions without files for clustered table",
+                        config.isEmptyBucketedPartitionsEnabled(),
                         true));
     }
 
@@ -139,6 +145,11 @@ public final class HiveSessionProperties
     public static boolean isMultiFileBucketingEnabled(ConnectorSession session)
     {
         return session.getProperty(MULTI_FILE_BUCKETING_ENABLED, Boolean.class);
+    }
+
+    public static boolean isEmptyBucketedPartitionsEnabled(ConnectorSession session)
+    {
+        return session.getProperty(EMPTY_BUCKETED_PARTITIONS_ENABLED, Boolean.class);
     }
 
     public static PropertyMetadata<DataSize> dataSizeSessionProperty(String name, String description, DataSize defaultValue, boolean hidden)

starburstdata/facebook-presto

presto-hive/src/main/java/com/facebook/presto/hive/BackgroundHiveSplitLoader.java
@@ -75,6 +75,7 @@ import static com.facebook.presto.hive.HiveErrorCode.HIVE_INVALID_BUCKET_FILES;
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_INVALID_METADATA;
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_INVALID_PARTITION_VALUE;
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_UNKNOWN_ERROR;
+import static com.facebook.presto.hive.HiveSessionProperties.isEmptyBucketedPartitionsEnabled;
 import static com.facebook.presto.hive.HiveSessionProperties.isForceLocalScheduling;
 import static com.facebook.presto.hive.HiveSessionProperties.isMultiFileBucketingEnabled;
 import static com.facebook.presto.hive.HiveUtil.checkCondition;
@@ -91,6 +92,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.util.concurrent.Futures.immediateFuture;
 import static java.lang.String.format;
+import static java.util.Collections.emptySet;
 import static java.util.Objects.requireNonNull;
 import static org.apache.hadoop.hive.common.FileUtils.HIDDEN_FILES_PATH_FILTER;
 
@@ -416,6 +418,13 @@ public class BackgroundHiveSplitLoader
 
             if (files.size() == bucketCount) {
                 groupedFiles = getBucketFilesSetsSingle(files);
+            }
+            else if (files.size() == 0 && isEmptyBucketedPartitionsEnabled(session)) {
+                ImmutableList.Builder<Set<LocatedFileStatus>> emptyBuckets = ImmutableList.builder();
+                for (int i = 0; i < bucketCount; ++i) {
+                    emptyBuckets.add(emptySet());
+                }
+                groupedFiles = emptyBuckets.build();
             }
             else if (isMultiFileBucketingEnabled(session)) {
                 groupedFiles = getBucketFilesSetsMulti(files);

presto-hive/src/main/java/com/facebook/presto/hive/HiveSessionProperties.java
@@ -74,6 +74,7 @@ public final class HiveSessionProperties
     private static final String IGNORE_CORRUPTED_STATISTICS = "ignore_corrupted_statistics";
     private static final String COLLECT_COLUMN_STATISTICS_ON_WRITE = "collect_column_statistics_on_write";
     private static final String MULTI_FILE_BUCKETING_ENABLED = "multi_file_bucketing_enabled";
+    private static final String EMPTY_BUCKETED_PARTITIONS_ENABLED = "empty_bucketed_partitions_enabled";
 
     private final List<PropertyMetadata<?>> sessionProperties;
 
@@ -284,6 +285,11 @@ public final class HiveSessionProperties
                         MULTI_FILE_BUCKETING_ENABLED,
                         "Allow multiple files per bucket for clustered table",
                         hiveClientConfig.isMultiFileBucketingEnabled(),
+                        true),
+                booleanProperty(
+                        EMPTY_BUCKETED_PARTITIONS_ENABLED,
+                        "Allow partitions without files for clustered table",
+                        hiveClientConfig.isEmptyBucketedPartitionsEnabled(),
                         true));
     }
 
@@ -471,6 +477,11 @@ public final class HiveSessionProperties
     public static boolean isMultiFileBucketingEnabled(ConnectorSession session)
     {
         return session.getProperty(MULTI_FILE_BUCKETING_ENABLED, Boolean.class);
+    }
+
+    public static boolean isEmptyBucketedPartitionsEnabled(ConnectorSession session)
+    {
+        return session.getProperty(EMPTY_BUCKETED_PARTITIONS_ENABLED, Boolean.class);
     }
 
     public static PropertyMetadata<DataSize> dataSizeSessionProperty(String name, String description, DataSize defaultValue, boolean hidden)